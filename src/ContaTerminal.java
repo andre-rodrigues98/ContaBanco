9584a1feb6304d50c82382c1a5a6c65e2b72b41a
Andre <andrermax111@gmail.com>
2022-08-09 23:49:30 -0300

    feat:adicao de impressão de mensagens para cliente e scanner de inputs do usuario

--- src/ContaTerminal.java
+++ src/ContaTerminal.java
@@ -7,6 +7,17 @@ public class ContaTerminal {
         double saldo;
         Scanner sc = new Scanner(System.in);
 
-        
+        System.out.println("Por favor, digite o seu nome");
+        nomeCliente = sc.nextLine();
+        System.out.println("Por favor, digite a sua Agência !");
+        agencia = sc.nextLine();
+        System.out.println("Por favor, digite o número da Agência !");
+        numero = sc.nextInt();
+        System.out.println("Por favor, digite o seu saldo");
+        saldo = sc.nextDouble();
+
+        System.out.println("Olá "+ nomeCliente +", obrigado por criar uma conta em nosso banco, sua agência é " + agencia +", conta " + numero +" e seu saldo " + saldo +" já está disponível para saque.");
+
+
     }
 }
