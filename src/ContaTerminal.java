cc676ca97ffc1f5564607b1980f86b68ce6fd55d
Andre <andrermax111@gmail.com>
2022-08-09 23:28:48 -0300

    feat: adicao de variaveis e classe scanner

--- src/ContaTerminal.java
+++ src/ContaTerminal.java
@@ -1,5 +1,12 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        int numero;
+        String agencia, nomeCliente;
+        double saldo;
+        Scanner sc = new Scanner(System.in);
+
+        
     }
 }
